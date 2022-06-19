b9040acdf7264bf88ec965962608dec937b0dab9
laurencia7 <patricialaurencia7@gmail.com>
2022-06-19 15:16:09 +0700

    projek mata kuliah human and computer interaction

---
+++ Assets/Script/EXIT.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EXIT : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("APLIKASI KELUAR");
+    }
+}
---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject PanelInfoo;
+    public AudioSource PlaySound;
+
+    public bool show = false;
+
+    public void ShowHideInfo()
+    {
+        if(!show)
+        {
+            PanelInfoo.SetActive(true);
+            show = true;
+        }
+        else
+        {
+            PanelInfoo.SetActive(false);
+            show = false;
+        }
+    }
+
+    public void PlaySoundInfo()
+    {
+        PlaySound.Play();
+    }
+}
